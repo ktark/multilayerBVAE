52e44d7c48db5f48558117f7ebab4c767a8a312b
Kaarel Tark <kaareltark@gmail.com>
2022-01-28 09:20:16 +0200

    encoder_decoder_dropout

--- src/modules.py
+++ src/modules.py
@@ -86,19 +86,19 @@ class BoxHeadSmallEncoder(nn.Module):
         self.encoder = nn.Sequential(
             nn.Conv2d(self.nc, 64, 4, 2, padding="valid"),  # 1          # B,  32, 32, 64
             nn.LeakyReLU(0.3),
-            nn.Dropout(p=0.3),
+            nn.Dropout(p=0.1),
             nn.BatchNorm2d(64),
             nn.Conv2d(64, 128, 4, 2, padding="valid"),  # 1          # B,  16, 16, 128
             nn.LeakyReLU(0.3),
-            nn.Dropout(p=0.3),
+            nn.Dropout(p=0.1),
             nn.BatchNorm2d(128),
             nn.Conv2d(128, 256, 2, 2, padding="valid"),  # B,   8,  8, 256
             nn.LeakyReLU(0.3),
-            nn.Dropout(p=0.3),
+            nn.Dropout(p=0.1),
             nn.BatchNorm2d(256),
             nn.Conv2d(256, 512, 2, 2, padding="valid"),  # B,  4,  4,  512
             nn.LeakyReLU(0.3),
-            nn.Dropout(p=0.3),
+            nn.Dropout(p=0.1),
             nn.BatchNorm2d(512),
             View((-1, 512 * 3 * 3)),  # B, 2048
             nn.Linear(4608, 1024),  # B, 1024
@@ -119,30 +119,38 @@ class BoxHeadSmallDecoder(nn.Module):
         self.latent_dim = latent_dim
         self.decoder = nn.Sequential(
             nn.Linear(self.latent_dim, 1024),  # B, 1024
+            nn.Dropout(p=0.3),
             nn.BatchNorm1d(1024),
             nn.Linear(1024, 4 * 4 * 512),  # B, 256
+            nn.Dropout(p=0.3),
             nn.BatchNorm1d(4 * 4 * 512),
             View((-1, 512, 4, 4)),  # B,  32,  4,  4
             nn.ConvTranspose2d(512, 256, 4, 2, 1),
             nn.LeakyReLU(0.3),
+            nn.Dropout(p=0.1),
             nn.BatchNorm2d(256),
 
             nn.ConvTranspose2d(256, 256, 4, 1, 1),
             nn.LeakyReLU(0.3),
+            nn.Dropout(p=0.1),
             nn.BatchNorm2d(256),
 
             nn.ConvTranspose2d(256, 128, 4, 2, 2),
             nn.LeakyReLU(0.3),
+            nn.Dropout(p=0.1),
             nn.BatchNorm2d(128),
 
             nn.ConvTranspose2d(128, 128, 4, 2, 1),
             nn.LeakyReLU(0.3),
+            nn.Dropout(p=0.1),
             nn.BatchNorm2d(128),
 
             nn.ConvTranspose2d(128, 64, 4, 1, 2),
             nn.LeakyReLU(0.3),
+            nn.Dropout(p=0.1),
             nn.ConvTranspose2d(64, 3, 4, 2),
-            nn.LeakyReLU(0.3)
+            nn.LeakyReLU(0.1),
+            nn.Dropout(p=0.1),
 
         )
 
